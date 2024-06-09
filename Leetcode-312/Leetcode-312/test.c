ed45c752ddc45dd54e11ae1e6a485f2063e7c0b8
JohnKingW <2067567633@qq.com>
2024-06-09 21:15:43 +0800

    *

---
+++ Leetcode-312/Leetcode-312/test.c
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+
+
+void InsertSortIndex(int** a, int n)
+{
+	for (int i = 0; i < n + 1; i++)
+	{
+		int end = i;
+		int tmp = end + 1;
+		while (end >= 0)
+		{
+			if()
+		}
+	}
+}
+
+
+void QuickSortIndex(int** a, int left, int right)
+{
+	if (left >= right)
+	{
+		return;
+	}
+	if (right - left + 1 < 10)
+	{
+		InsertSort(a + left, right - left + 1);
+	}
+}
+
+
+int maxCoins(int* nums, int numsSize) {
+
+}
+
+
+int main()
+{
+
+}
